f986e1ebb58842d9313e1e507e205370bd62e932
Jordan Tran <thai.jordan@gmail.com>
2023-02-14 16:29:51 -0800

    NavBar in the works

--- src/Nav.js
+++ src/Nav.js
@@ -1,3 +1,25 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./index.css";
+
 export default function Nav() {
-    
+    <header>
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">
+                    Fetch Rewards
+                </NavLink>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav> 
+    </header>
 };
